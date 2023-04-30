250eec972449f135fd35c9cb710a27caf4d36e83
LionyxML <rahul.juliato@gmail.com>
2023-04-30 11:40:35 -0300

    feat: integation tests with puppeteer

---
+++ book/03_example_express_site/integration-tests/basic-navigation.test.js
@@ -0,0 +1,33 @@
+const portfinder = require("portfinder");
+const puppeteer = require("puppeteer");
+
+const app = require("../index.js");
+
+let server = null;
+let port = null;
+
+beforeEach(async () => {
+  port = await portfinder.getPortPromise();
+  server = app.listen(port);
+});
+
+afterEach(() => {
+  server.close();
+});
+
+test("home paage links to about page", async () => {
+  const browser = await puppeteer.launch({
+    // Specifics for macOS
+    executablePath: "/Applications/Chromium.app/Contents/MacOS/Chromium",
+    headless: false,
+  });
+  const page = await browser.newPage();
+  await page.goto(`http://localhost:${port}`);
+  await Promise.all([
+    page.waitForNavigation(),
+    page.click('[data-test-id="about"]'),
+  ]);
+
+  expect(page.url()).toBe(`http://localhost:${port}/about`);
+  await browser.close();
+});
